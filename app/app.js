248492c549820cdbb6af1b7bdd18256fab5a61c4
Placidchills <placidchills@gmail.com>
2026-06-04 22:43:06 +0530

    Initial Node.js application

---
+++ app/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/",(req,res)=> {
+    res.json({
+      status : "autonomous release platform"
+  });
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "healthy"
+    });
+});
+
+app.listen(PORT,()=> {
+    console.log(`Server running on port ${PORT}`);
+});
